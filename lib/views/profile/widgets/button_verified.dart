a0a152e8ea6e9476e66ab5bfc9e6fe5a6e2be349
Muhammad Fadli Hasan <winnerfadli999@gmail.com>
2023-03-31 23:27:50 +0700

    change logo and update link verified

--- lib/views/profile/widgets/button_verified.dart
+++ lib/views/profile/widgets/button_verified.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/utils/color_constants.dart';
+import 'package:mobile/utils/open_link.dart';
 import 'package:mobile/widgets/text_styles.dart';
 
 class ButtonVerified extends StatelessWidget {
@@ -9,27 +10,32 @@ class ButtonVerified extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: ColorConstants.error,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          padding: EdgeInsets.symmetric(vertical: 9, horizontal: 14),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Verify your account before\nmaking a campaign!',
-                style: body6TextStyle(
-                  weight: FontWeight.bold,
-                  color: ColorConstants.slate[25],
+        GestureDetector(
+          onTap: () {
+            openLink(Uri.parse("https://forms.gle/A4diJ8NJ5MpaTpkB9"));
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: ColorConstants.error,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 9, horizontal: 14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Verify your account before\nmaking a campaign!',
+                  style: body6TextStyle(
+                    weight: FontWeight.bold,
+                    color: ColorConstants.slate[25],
+                  ),
                 ),
-              ),
-              Icon(
-                Icons.chevron_right,
-                color: Colors.white,
-              ),
-            ],
+                Icon(
+                  Icons.chevron_right,
+                  color: Colors.white,
+                ),
+              ],
+            ),
           ),
         ),
         SizedBox(height: 15),
